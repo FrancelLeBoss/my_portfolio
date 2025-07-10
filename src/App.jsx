112daff1e058b9ac530312da09ca884271782113
Francel Cabrel Pehuie <cabrelprowo@gmail.com>
2025-07-10 10:21:35 -0400

    SEO optimised

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,31 @@ import About from "./components/About";
 import Project from "./components/Project";
 import Contact from "./components/Contact";
 import "./i18n";
+import { Helmet } from "react-helmet";
+import { useTranslation } from "react-i18next";
 
 function App() {
+  const { t, i18n } = useTranslation();
+
   return (
     <div className="font-sans bg-backgroundLight dark:bg-backgroundDark text-secondary dark:text-primary">
+      <Helmet>
+        <html lang={i18n.language} />
+        <title>{t("seo.title")}</title>
+        <meta name="description" content={t("seo.description")} />
+        <meta name="keywords" content={t("seo.keywords")} />
+        <meta name="author" content="Francel Prowo" />
+
+        {/* Open Graph meta tags */}
+        <meta property="og:title" content={t("seo.ogTitle")} />
+        <meta property="og:description" content={t("seo.ogDescription")} />
+        <meta
+          property="og:image"
+          content="https://www.francel-prowo.com/og-image.jpg"
+        />
+        <meta property="og:url" content="https://www.francel-prowo.com/" />
+      </Helmet>
+
       <Navbar />
       <div className="scroll-smooth">
         <Hero />
